5e569cc62d920e25f8e0f0724c03733052d4c57a
volada095-dotcom <volada095@gmail.com>
2026-01-12 15:08:35 +0200

    Create course1.js

---
+++ data/course1.js
@@ -0,0 +1,23 @@
+const courseData = {
+    course1: {
+        title: "30-Day Advanced Phrasal Verbs Challenge",
+        days: [
+            {
+                title: "Day 1 - Basic Phrasal Verbs",
+                content: "Advanced phrasal verbs challenge 2 day 1 explanation ...",
+                videos: [
+                    { url: "https://drive.google.com/file/d/16H5itgQA_ql6iVfQUED66sgNUO8X6KV_/preview" }
+                ],
+                exercises: [
+                    {
+                        name: "Day 1 Exercise",
+                        type: "exercise",
+                        questions: [
+                            { question: "Tell the children to ___ (be careful)...", answer: "mind out" }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+};
